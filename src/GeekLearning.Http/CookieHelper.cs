29a67ff8dd86ca13c5b1eeda828eb4b75ee891a6
Cyprien Autexier <sandorfr@users.noreply.github.com>
2018-03-07 16:50:01 -0800

    per request-cookie container

---
+++ src/GeekLearning.Http/CookieHelper.cs
@@ -0,0 +1,33 @@
+﻿namespace GeekLearning.Http
+{
+        using Microsoft.Net.Http.Headers;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+
+    // http://www.stefanhendriks.com/2016/05/11/integration-testing-your-asp-net-core-app-dealing-with-anti-request-forgery-csrf-formdata-and-cookies/
+    public static class CookiesHelper
+        {
+
+            public static void ExtractCookiesFromResponse(this HttpResponseMessage response, IDictionary<string, string> cookies)
+            {
+                if (response.Headers.TryGetValues("Set-Cookie", out IEnumerable<string> values))
+                {
+                    SetCookieHeaderValue.ParseList(values.ToList()).ToList().ForEach(cookie =>
+                    {
+                        cookies[cookie.Name.Value] =  cookie.Value.Value;
+                    });
+                }
+            }
+
+            public static HttpRequestMessage PutCookiesOnRequest(this HttpRequestMessage request, IDictionary<string, string> cookies)
+            {
+                cookies.Keys.ToList().ForEach(key =>
+                {
+                    request.Headers.Add("Cookie", new CookieHeaderValue(key, cookies[key]).ToString());
+                });
+
+                return request;
+            }
+        }
+}
---
+++ src/GeekLearning.Http/RequestCookieHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace GeekLearning.Http
+{
+    public class RequestCookieHandler: DelegatingHandler
+    {
+        private readonly string backingProperty ;
+
+        public RequestCookieHandler(HttpMessageHandler innerHandler): base(innerHandler)
+        {
+            this.backingProperty = "x-cookies";
+        }
+
+        public RequestCookieHandler(HttpMessageHandler innerHandler, string backingProperty) : base(innerHandler)
+        {
+            this.backingProperty = backingProperty;
+        }
+
+        protected override async System.Threading.Tasks.Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            var cookies = request.Properties[this.backingProperty] as IDictionary<string, string>;
+            request.PutCookiesOnRequest(cookies);
+            var response = await base.SendAsync(request, cancellationToken);
+            response.ExtractCookiesFromResponse(cookies);
+            return response;
+        }
+    }
+}
