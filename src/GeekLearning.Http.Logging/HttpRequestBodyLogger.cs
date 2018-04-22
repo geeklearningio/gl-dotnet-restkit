541f5551129c4bcf5bf61e44eff8b58edcfc2306
Cyprien Autexier <2660262+sandorfr@users.noreply.github.com>
2018-04-22 16:49:17 +1000

    add request logger with custom sink

---
+++ src/GeekLearning.Http.Logging/HttpRequestBodyLogger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+using GeekLearning.D64;
+using System.Diagnostics;
+
+namespace GeekLearning.Http.Logging
+{
+    public class HttpRequestBodyLogger : DelegatingHandler
+    {
+        private readonly ILogSink sink;
+
+        public HttpRequestBodyLogger(HttpMessageHandler innerHandler, ILogSink sink) : base(innerHandler)
+        {
+            this.sink = sink;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var requestLog = new RequestLog
+            {
+                RequestHeaders = request.Headers,
+                Uri = request.RequestUri.ToString(),
+                Method = request.Method.ToString(),
+                ActivityId = Activity.Current.Id,
+                ParentActivityId = Activity.Current.ParentId,
+            };
+
+            if (request.Content != null)
+            {
+                await request.Content.LoadIntoBufferAsync();
+                requestLog.RequestBody = await request.Content.ReadAsStringAsync();
+            }
+            var response = await base.SendAsync(request, cancellationToken);
+            await response.Content.LoadIntoBufferAsync();
+            requestLog.ResponseBody = await response.Content.ReadAsStringAsync();
+
+            this.sink.Post(requestLog);
+            return response;
+        }
+
+    }
+}
---
+++ src/GeekLearning.Http.Logging/RequestLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace GeekLearning.Http.Logging
+{
+    public class RequestLog
+    {
+        public string ParentActivityId { get; set; }
+        public string ActivityId { get; set; }
+        public string Method { get; set; }
+        public string Uri { get; set; }
+        public HttpHeaders RequestHeaders { get; set; }
+        public string RequestBody { get; set; }
+        public HttpHeaders ResponseHeaders { get; set; }
+        public string ResponseBody { get; set; }
+    }
+}
